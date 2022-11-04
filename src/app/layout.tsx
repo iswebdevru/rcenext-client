97e3acafe13b272ff86d96131184580b17592b61
wardxela <axelwardweb@gmail.com>
2022-11-04 16:25:45 +0300

    feat: add tailwindcss support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from '../components/Header';
+import '../styles/globals.css';
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html>
       <head></head>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,6 @@ function groupToString({ name, index, course }: Group) {
   return `${name}-${course}${index < 10 ? `0${index}` : index}`;
 }
 
-async function fetchGroups() {
-  return fetch('http://localhost:3000/groups').then(res =>
-    res.json()
-  ) as Promise<Group[]>;
-}
-
-export default async function Index() {
-  const groups = await fetchGroups();
-
-  return (
-    <ul>
-      {groups.map(group => {
-        return <li key={group.id}>{groupToString(group)}</li>;
-      })}
-    </ul>
-  );
+export default function Index() {
+  return <div>home</div>;
 }
