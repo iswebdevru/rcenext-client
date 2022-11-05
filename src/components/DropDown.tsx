8236e38b215343c2829caa3b5f36b049bbc9eabe
wardxela <axelwardweb@gmail.com>
2022-11-05 14:03:51 +0300

    refactor: improve accessibility of DropDown component, change behavior a bit

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,29 +1,48 @@
 'use client';
 
 import classNames from 'classnames';
-import { ChangeEventHandler, useEffect, useId, useState } from 'react';
+import React, { ChangeEventHandler, useEffect, useRef, useState } from 'react';
 
 export interface Option {
-  id: number;
+  id: number | null;
   value: string;
 }
 
 export interface DropDownProps {
   options: Option[];
-  selected: number | null;
+  selected: Option['id'];
   select: (optionId: number | null) => void;
+}
+
+const defaultOption: Option = {
+  id: null,
+  value: '-',
+};
+
+function sortByMostAccurate(searchText: string, a: string, b: string) {
+  const searchLower = searchText.toLowerCase();
+  const aMatch = a.toLowerCase().startsWith(searchLower);
+  const bMatch = b.toLowerCase().startsWith(searchLower);
+  if (aMatch && bMatch) {
+    return a.length - b.length;
+  } else if (!aMatch && bMatch) {
+    return 1;
+  } else if (aMatch && !bMatch) {
+    return -1;
+  }
+  return 0;
 }
 
 export default function DropDown({ options, select, selected }: DropDownProps) {
   const [searchText, setSearchText] = useState('');
-  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [showOptions, setShowOptions] = useState(false);
   const [shouldSort, setShouldSort] = useState(false);
-  const id = useId();
+  const dropDownRef = useRef<HTMLDivElement | null>(null);
 
-  const handleClickFactory = (id: number | null) => () => {
+  const handleSelectFactory = (id: number | null) => () => {
     select(id);
     setSearchText(options.find(o => o.id === id)?.value || '');
-    setShowSuggestions(false);
+    setShowOptions(false);
   };
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = event => {
@@ -39,28 +58,18 @@ export default function DropDown({ options, select, selected }: DropDownProps) {
     }
   };
 
-  const sortedOptions = shouldSort
-    ? options.sort((a, b) => {
-        const search = searchText.toLowerCase();
-        const aMatch = a.value.toLowerCase().startsWith(search);
-        const bMatch = b.value.toLowerCase().startsWith(search);
-        if (aMatch && bMatch) {
-          return a.value.length - b.value.length;
-        } else if (!aMatch && bMatch) {
-          return 1;
-        } else if (aMatch && !bMatch) {
-          return -1;
-        }
-        return 0;
-      })
-    : options;
+  const handleKeyDownFactory =
+    (id: number | null) => (event: React.KeyboardEvent<HTMLLIElement>) => {
+      if (event.key === 'Enter') {
+        handleSelectFactory(id)();
+      }
+    };
 
+  // Handle outside click
   useEffect(() => {
-    document.body.addEventListener('click', event => {
-      const el = (event.target as Element)?.closest(`[data-id="${id}"]`);
-      const elWithFocus = document.activeElement?.closest(`[data-id="${id}"]`);
-      if (!el && !elWithFocus) {
-        setShowSuggestions(false);
+    document.body.addEventListener('mousedown', event => {
+      if (!dropDownRef.current?.contains(event.target as Node)) {
+        setShowOptions(false);
       }
     });
   }, []);
@@ -69,43 +78,42 @@ export default function DropDown({ options, select, selected }: DropDownProps) {
     setShouldSort(false);
   }, [searchText]);
 
+  const optionsWithDefault = [
+    defaultOption,
+    ...(shouldSort
+      ? options.sort((a, b) => sortByMostAccurate(searchText, a.value, b.value))
+      : options),
+  ];
+
   return (
-    <div className="relative w-56" data-id={id}>
+    <div className="relative w-56" ref={dropDownRef}>
       <input
         type="text"
         className="border bg-gray-50 w-full p-3 focus:bg-gray-100 outline-0"
         value={searchText}
         onChange={handleChange}
-        onFocus={() => setShowSuggestions(true)}
+        onFocus={() => setShowOptions(true)}
       />
       <div
         className={classNames({
-          'top-full left-0 right-0 absolute duration-300 transition-[height] origin-top overflow-auto border-x bg-white':
+          'top-full left-0 right-0 absolute duration-300 transition-[height] origin-top overflow-auto border-x bg-white scrollbar-thumb-gray-200 scrollbar-thin scrollbar-track-gray-50 hover:scrollbar-thumb-gray-300':
             true,
-          'h-0 border-0': !showSuggestions,
-          'h-72  border-b': showSuggestions,
+          'h-0 border-0': !showOptions,
+          'h-72  border-b': showOptions,
         })}
       >
         <ul>
-          <li
-            className={classNames({
-              'cursor-pointer border-t p-3 transition': true,
-              'bg-red-50 hover:bg-red-50': selected === null,
-              'hover:bg-gray-50': selected !== null,
-            })}
-            onClick={handleClickFactory(null)}
-          >
-            -
-          </li>
-          {sortedOptions.map(({ id, value }) => (
+          {optionsWithDefault.map(({ id, value }) => (
             <li
               className={classNames({
-                'cursor-pointer border-t p-3 transition': true,
-                'bg-green-50 hover:bg-green-50': selected === id,
+                'cursor-pointer select-none border-t p-3 transition': true,
+                'bg-emerald-100 hover:bg-emerald-100': selected === id,
                 'hover:bg-gray-50': selected !== id,
               })}
               key={id}
-              onClick={handleClickFactory(id)}
+              tabIndex={0}
+              onClick={handleSelectFactory(id)}
+              onKeyDown={handleKeyDownFactory(id)}
             >
               {value}
             </li>
