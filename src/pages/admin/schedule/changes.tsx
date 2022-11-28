56c3e822912b62433d753d1d235be197114b7344
wardxela <axelwardweb@gmail.com>
2022-11-28 18:45:25 +0300

    start to develop schedule page

--- src/pages/admin/schedule/changes.tsx
+++ src/pages/admin/schedule/changes.tsx
@@ -1,31 +1,24 @@
+import Schedule from '../../../components/features/Schedule';
 import AdminNav from '../../../components/layout/AdminNav';
 import Layout from '../../../components/layout/Layout';
+import SideBar from '../../../components/layout/SideBar';
 
 export default function Changes() {
   return (
     <Layout>
       <AdminNav />
-      <div className="grid flex-1 grid-cols-4 gap-3">
-        <div>
-          <div>
-            <h6>Группа</h6>
-            <table>
-              <tbody>
-                <tr>
-                  <th>№</th>
-                  <th>Предмет</th>
-                  <th>Кабинет</th>
-                </tr>
-                <tr>
-                  <td>0</td>
-                  <td>Математика</td>
-                  <td>321</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+      <div className="flex-1">
+        <div></div>
+        <div className="grid grid-cols-4 gap-3">
+          <Schedule />
+          <Schedule />
+          <Schedule />
+          <Schedule />
+          <Schedule />
+          <Schedule />
         </div>
       </div>
+      <SideBar title="Фильтры">фыва</SideBar>
     </Layout>
   );
 }
