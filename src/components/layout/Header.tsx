52b5358f735b35f0844e46ed94d0ecb68b990040
wardxela <axelwardweb@gmail.com>
2022-11-30 14:53:42 +0300

    feat: make header responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,43 +1,85 @@
 import Link, { LinkProps } from 'next/link';
 import Logo from '../icons/Logo';
 import ThemeButton from '../features/ThemeButton';
-import { PropsWithChildren } from 'react';
-
-const HeaderLink = (props: PropsWithChildren<LinkProps>) => {
-  return (
-    <Link
-      {...props}
-      className="transition-colors font-medium text-sm text-zinc-700 hover:text-accent-600 dark:text-zinc-100 dark:hover:text-accent-500"
-    />
-  );
-};
+import { PropsWithChildren, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames';
 
 export default function Header() {
+  const [isOpened, setIsOpened] = useState(false);
+
   return (
-    <header className="fixed bottom-0 left-0 top-0 right-0 z-50 h-[var(--header-height)] transition-colors border-b common-border component-bg">
-      <div className="h-full max-w-screen-2xl mx-auto px-3 flex flex-row items-center gap-40">
+    <header className="fixed left-0 top-0 right-0 z-50 h-[var(--header-height)] transition-colors border-b common-border component-bg">
+      <div className="flex flex-row items-center justify-between h-full gap-40 px-3 mx-auto sm:justify-start max-w-screen-2xl">
         <Link href="/" className="group">
           <Logo />
         </Link>
-        <div className="flex flex-row items-center justify-between flex-1">
-          <nav>
-            <ul className="flex gap-8 text-sm flex-row">
-              <li>
-                <HeaderLink href="/admin">Админ</HeaderLink>
-              </li>
-              <li>
-                <HeaderLink href="/admin">Админ</HeaderLink>
-              </li>
-              <li>
-                <HeaderLink href="/admin">Админ</HeaderLink>
-              </li>
+        <div className="flex flex-row items-center justify-between gap-4 sm:flex-1">
+          <nav className="hidden sm:block">
+            <ul className="flex items-center gap-10">
+              <HeaderLinks />
             </ul>
           </nav>
-          <div className="flex items-center">
-            <ThemeButton />
-          </div>
+          <ThemeButton />
+          <button className="sm:hidden" onClick={() => setIsOpened(true)}>
+            <FontAwesomeIcon icon={faBars} fixedWidth size="2x" />
+          </button>
+        </div>
+      </div>
+      <div
+        className={classNames({
+          'transition block sm:hidden fixed top-0 bottom-0 left-0 right-0 bg-zinc-900 dark:bg-black ':
+            true,
+          'pointer-events-auto bg-opacity-25 backdrop-blur-[1px] dark:bg-opacity-25':
+            isOpened,
+          'pointer-events-none bg-opacity-0 dark:bg-opacity-0': !isOpened,
+        })}
+        onClick={e => {
+          if (e.currentTarget === e.target) {
+            setIsOpened(false);
+          }
+        }}
+      >
+        <div
+          className={classNames({
+            'w-2/3 h-full component-bg transition': true,
+            'translate-x-0': isOpened,
+            '-translate-x-full': !isOpened,
+          })}
+        >
+          <nav>
+            <ul>
+              <HeaderLinks />
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
   );
 }
+
+function HeaderLinks() {
+  return (
+    <>
+      <li>
+        <HeaderLink href="/admin">Админ</HeaderLink>
+      </li>
+      <li>
+        <HeaderLink href="/admin">Админ</HeaderLink>
+      </li>
+      <li>
+        <HeaderLink href="/admin">Админ</HeaderLink>
+      </li>
+    </>
+  );
+}
+
+const HeaderLink = (props: PropsWithChildren<LinkProps>) => {
+  return (
+    <Link
+      {...props}
+      className="text-sm font-medium transition-colors text-zinc-700 hover:text-accent-600 dark:text-zinc-100 dark:hover:text-accent-500"
+    />
+  );
+};
--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -19,20 +19,20 @@ export default function SideBar({
     <aside
       className={classNames({
         [className ? className : '']: className,
-        'z-40 p-3 fixed w-full h-[80vh] left-0 transition-[top] sm:sticky sm:left-auto sm:top-[calc(var(--header-height)+1rem)] bg-white bg-opacity-95 dark:bg-zinc-900 dark:bg-opacity-95 sm:bg-transparent sm:p-0 sm:w-auto sm:h-[calc(100vh-var(--header-height)-2rem)] shrink-0 grow-1':
+        'z-40 p-3 fixed w-full h-[80vh] left-0 transition-[top] sm:sticky sm:left-auto sm:top-[calc(var(--header-height)+1rem)] bg-zinc-100 bg-opacity-95 dark:bg-zinc-900 dark:bg-opacity-95 sm:bg-transparent sm:p-0 sm:w-auto sm:h-[calc(100vh-var(--header-height)-2rem)] shrink-0 grow-1':
           true,
         'top-[20vh]': opened,
         'top-[calc(100vh-82px)]': !opened,
       })}
     >
-      <div className="sm:hidden sticky top-0 z-10 mb-4">
+      <div className="sticky top-0 z-10 mb-4 sm:hidden">
         <button
           aria-label="Reveal sidebar"
-          className="flex items-center flex-col border common-border py-2 w-full rounded-md component-bg"
+          className="flex flex-col items-center w-full py-2 border rounded-md common-border component-bg"
           onClick={() => setOpened(prev => !prev)}
         >
-          <span className="common-text font-bold text-lg mb-2">{title}</span>
-          <span className="block w-8 h-1 bg-neutral-800 dark:bg-neutral-200 rounded-md"></span>
+          <span className="mb-2 text-lg font-bold common-text">{title}</span>
+          <span className="block w-8 h-1 rounded-md bg-neutral-800 dark:bg-neutral-200"></span>
         </button>
       </div>
       <div className="common-scrollbar overflow-auto h-[calc(80vh-98px)] sm:h-full">
